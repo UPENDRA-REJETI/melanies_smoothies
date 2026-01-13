2ecde1723743d7d0e8fa86c594321b46e3541e1c
Upendra Rejeti <upendrarejeti06@gmail.com>
2026-01-13 13:37:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,21 +18,28 @@ from snowflake.snowpark.functions import col
 cnx=st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"), col("SEARCH_ON"))
+
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:",
     my_dataframe.select(col("FRUIT_NAME")).to_pandas()["FRUIT_NAME"].tolist(),
-    my_dataframe, max_selections=5
+    max_selections=5
 )
+
 if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
       ingredients_string += fruit_chosen+' '
       st.subheader(fruit_chosen+' Nutrition Information')
+
       search_on_value = session.table("smoothies.public.fruit_options") \
         .filter(col("FRUIT_NAME") == fruit_chosen) \
         .select(col("SEARCH_ON")) \
         .collect()[0][0]
-      smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+search_on_value)
+
+      smoothiefroot_response = requests.get(
+          "https://my.smoothiefroot.com/api/fruit/" + search_on_value.replace(" ", "%20")
+      )
+
       sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     st.write (ingredients_string)
@@ -46,5 +53,3 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 og_dataset = session.table("smoothies.public.orders")
-
-
