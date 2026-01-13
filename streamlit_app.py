7f56ce8a59c226eae0c135bf45935af3a4b5b6ef
Upendra Rejeti <upendrarejeti06@gmail.com>
2026-01-13 12:46:38 +0530

    Add SEARCH_ON column to fruit options selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,9 +17,10 @@ from snowflake.snowpark.functions import col
 
 cnx=st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"), col("SEARCH_ON"))
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:",
+    my_dataframe.select(col("FRUIT_NAME")).to_pandas()["FRUIT_NAME"].tolist(),
     my_dataframe, max_selections=5
 )
 if ingredients_list:
@@ -27,6 +28,10 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
       ingredients_string += fruit_chosen+' '
       st.subheader(fruit_chosen+' Nutrition Information')
+      search_on_value = session.table("smoothies.public.fruit_options") \
+        .filter(col("FRUIT_NAME") == fruit_chosen) \
+        .select(col("SEARCH_ON")) \
+        .collect()[0][0]
       smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_chosen)
       sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
